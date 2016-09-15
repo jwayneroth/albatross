7d360567d37b8bd9c06d35cdb71b02fa449b1bd5
Jonathan Roth <jwayneroth@gmail.com>
2016-09-14 22:00:57 -0400

    subscribe to timerUpdates and logs on audio session change

--- ios/AlbatrossPlayer/lib/JRMultiTrackPlayer.m
+++ ios/AlbatrossPlayer/lib/JRMultiTrackPlayer.m
@@ -14,30 +14,30 @@
 // init
 //
 -(id)init {
-	
+
 	self = [super init];
-	
+
 	if (self) {
 		_randID = arc4random_uniform(1000);
 		_players = [[NSMutableArray alloc] init];
 		_playerID = 0;
 	}
-	
+
 	// query if other audio is playing
 	BOOL isPlayingWithOthers = [[AVAudioSession sharedInstance] isOtherAudioPlaying];
-	
+
 	// test it with...
 	(isPlayingWithOthers) ? NSLog(@"other audio is playing") : NSLog(@"no other audio is playing");
-	
+
 	// set up AudioSession
 	AVAudioSession *session = [AVAudioSession sharedInstance];
-	
+
 	NSError *setCategoryError = nil;
-	
+
 	if (![session setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError]) {
 		NSLog(@"%@", [setCategoryError localizedDescription]);
 	}
-	
+
 	// register for notifications
 	[[NSNotificationCenter defaultCenter] addObserver:self
                                            selector:@selector(interruption:)
@@ -62,48 +62,48 @@ RCT_EXPORT_MODULE();
 // showMediaPicker
 //
 -(void) showMediaPicker:(NSInteger)playerID  {
-	
+
 	self.playerID = playerID;
-	
+
 	if(self.mediaPicker == nil) {
-		
+
 		self.mediaPicker = [[MPMediaPickerController alloc] initWithMediaTypes:MPMediaTypeAnyAudio];//MPMediaTypeMusic];
-	
+
 		[self.mediaPicker setDelegate:self];
 		[self.mediaPicker setAllowsPickingMultipleItems:NO];
 		[self.mediaPicker setShowsCloudItems:NO];
 		self.mediaPicker.prompt = @"Select song";
 	}
-	
+
 	AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-	
+
 	[delegate.rootViewController presentViewController:self.mediaPicker animated:YES completion:nil];
-	
+
 }
 
 //
 // mediaPicker
 //
--(void) mediaPicker:(MPMediaPickerController *)mediaPicker 
+-(void) mediaPicker:(MPMediaPickerController *)mediaPicker
   didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection {
-	
+
 	MPMediaItem *mediaItem = mediaItemCollection.items[0];
-	
+
 	NSURL *assetURL = [mediaItem valueForProperty:MPMediaItemPropertyAssetURL];
-	
+
 	NSDictionary *evtObject = @{@"artist": [mediaItem valueForProperty:MPMediaItemPropertyAlbumArtist],
                               @"title" : [mediaItem valueForProperty:MPMediaItemPropertyTitle],
                              @"player" : [NSNumber numberWithInteger:self.playerID]
 	};
-	
+
 	[self.bridge.eventDispatcher sendAppEventWithName:@"SongPlaying" body:evtObject];
-	                                                    
+
 	NSError *error;
 	AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:assetURL error:&error];
 	float rate;
 	float volume;
 	float pan;
-	
+
 	if ([self.players count] > self.playerID) {
 		AVAudioPlayer *oldPlayer = [self.players objectAtIndex:self.playerID];
 		rate = oldPlayer.rate;
@@ -111,52 +111,63 @@ RCT_EXPORT_MODULE();
 		pan = oldPlayer.pan;
 		[self.players replaceObjectAtIndex:self.playerID withObject:player];
 	} else {
-		rate = 1.0;                                                                                                                  
+		rate = 1.0;
 		volume = 1.0;
 		pan = 0;
 		[self.players addObject:player];
 	}
-	
+
 	[player setDelegate:self];
 	player.numberOfLoops = -1;
 	player.enableRate = YES;
 	player.rate = rate;
 	player.volume = volume;
 	player.pan = pan;
-	
+
 	if (error) {
 		NSLog(@"%@", [error localizedDescription]);
 	} else {
 		[player prepareToPlay];
 		[player play];
 		if (!self.timer) {
-			self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1 
-			                                              target:self 
-			                                            selector:@selector(timerUpdate) 
-			                                            userInfo:nil 
+			self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1
+			                                              target:self
+			                                            selector:@selector(timerUpdate)
+			                                            userInfo:nil
 			                                             repeats:YES];
 		}
 	}
-	
+
 	hideMediaPicker();
-	
+
 }
 
 //
 // timerUpdate
 //
 - (void)timerUpdate {
-	
+
 	//NSLog(@"timerUpdate");
-	
+
 	//float progress = self.audioPlayer.currentTime;
 	//[self.seekbar setValue:progress];
-	
+
+	NSMutableArray *evtObject = [[NSMutableArray alloc] init];
+	NSDictionary *active;
+	int i = 0;
+
 	for (AVAudioPlayer *player in self.players) {
 		if (player.playing == YES) {
+
 			//NSLog(@"player x is at %f", player.currentTime);
+
+			active = @{@"player": i,
+			      @"currentTime": player.currentTime};
 		}
+		i++;
 	}
+
+	[self.bridge.eventDispatcher sendAppEventWithName:@"PlayingUpdate" body:evtObject];
 }
 
 //
@@ -184,17 +195,17 @@ RCT_EXPORT_METHOD(showPicker:(NSInteger)playerID) {
 //
 // setRateAsync
 //
-RCT_EXPORT_METHOD(setRateAsync:(nonnull NSNumber*)rate 
+RCT_EXPORT_METHOD(setRateAsync:(nonnull NSNumber*)rate
                      forPlayer:(NSInteger)playerID
                       resolver:(RCTPromiseResolveBlock)resolve
                       rejecter:(RCTPromiseRejectBlock)reject) {
-	
+
 	AVAudioPlayer *player;
-	
+
 	if ([self.players count] > playerID) {
 		player = [self.players objectAtIndex:playerID];
 	}
-	
+
 	if (player) {
 		player.rate = [rate floatValue];
 		NSLog(@"setRate %f for player %@", [rate floatValue], [NSNumber numberWithInteger:playerID]);
@@ -205,17 +216,17 @@ RCT_EXPORT_METHOD(setRateAsync:(nonnull NSNumber*)rate
 //
 // setVolAsync
 //
-RCT_EXPORT_METHOD(setVolAsync:(nonnull NSNumber*)volume 
+RCT_EXPORT_METHOD(setVolAsync:(nonnull NSNumber*)volume
                      forPlayer:(NSInteger)playerID
                       resolver:(RCTPromiseResolveBlock)resolve
                       rejecter:(RCTPromiseRejectBlock)reject) {
-	
+
 	AVAudioPlayer *player;
-	
+
 	if ([self.players count] > playerID) {
 		player = [self.players objectAtIndex:playerID];
 	}
-	
+
 	if (player) {
 		player.volume = [volume floatValue];
 		resolve(@"set vol");
@@ -225,17 +236,17 @@ RCT_EXPORT_METHOD(setVolAsync:(nonnull NSNumber*)volume
 //
 // setPanAsync
 //
-RCT_EXPORT_METHOD(setPanAsync:(nonnull NSNumber*)pan 
+RCT_EXPORT_METHOD(setPanAsync:(nonnull NSNumber*)pan
                      forPlayer:(NSInteger)playerID
                       resolver:(RCTPromiseResolveBlock)resolve
                       rejecter:(RCTPromiseRejectBlock)reject) {
-	
+
 	AVAudioPlayer *player;
-	
+
 	if ([self.players count] > playerID) {
 		player = [self.players objectAtIndex:playerID];
 	}
-	
+
 	if (player) {
 		player.pan = [pan floatValue];
 		resolve(@"set pan");
@@ -248,23 +259,23 @@ RCT_EXPORT_METHOD(setPanAsync:(nonnull NSNumber*)pan
 RCT_EXPORT_METHOD(stopPlayerByID:(NSInteger)playerID
                       resolver:(RCTPromiseResolveBlock)resolve
                       rejecter:(RCTPromiseRejectBlock)reject) {
-	
+
 	AVAudioPlayer *player;
-	
+
 	if ([self.players count] > playerID) {
 		player = [self.players objectAtIndex:playerID];
 	}
-	
+
 	if (player) {
-		
+
 		[player stop];
 		player.currentTime = 0;
-		player.rate = 1.0;                                                                                                                  
+		player.rate = 1.0;
 		player.volume = 1.0;
 		player.pan = 0;
 		//[self.players removeObjectAtIndex:playerID];
 		resolve(@"stopped player");
-		
+
 		// if no players playing, kill our timer
 		BOOL playing = NO;
 		for (AVAudioPlayer *player in self.players) {
@@ -285,34 +296,34 @@ RCT_EXPORT_METHOD(stopPlayerByID:(NSInteger)playerID
 RCT_EXPORT_METHOD(findAlbatross:(NSInteger)playerID
                        resolver:(RCTPromiseResolveBlock)resolve
                        rejecter:(RCTPromiseRejectBlock)reject) {
-	
+
 	NSLog(@"findAlbatross::%zd", self.randID);
-	
+
 	MPMediaQuery *songsQuery = [[MPMediaQuery alloc] init];
 	NSArray *items = [songsQuery items];
-	
+
 	for(MPMediaItem *item in items) {
-		
+
 		NSString *albumArtist =[item valueForProperty: MPMediaItemPropertyAlbumArtist];
 		NSString *title = [item valueForProperty: MPMediaItemPropertyTitle];
-		
+
 		if([albumArtist isEqualToString:@"Fleetwood Mac"] && [title isEqualToString:@"Albatross"]) {
-		
+
 			NSURL *assetURL = [item valueForProperty:MPMediaItemPropertyAssetURL];
-			
+
 			NSDictionary *evtObject = @{@"artist": albumArtist,
                                   @"title" : title,
                                  @"player" : [NSNumber numberWithInteger:playerID]
 			};
-	
-	[self.bridge.eventDispatcher sendAppEventWithName:@"SongPlaying" body:evtObject];
-			
+
+			[self.bridge.eventDispatcher sendAppEventWithName:@"SongPlaying" body:evtObject];
+
 			NSError *error;
 			AVAudioPlayer *player;
-			
+
 			player = [[AVAudioPlayer alloc] initWithContentsOfURL:assetURL error:&error];
 			float rate;
-			
+
 			if ([self.players count] > self.playerID) {
 				AVAudioPlayer *oldPlayer = [self.players objectAtIndex:self.playerID];
 				rate = oldPlayer.rate;
@@ -321,53 +332,56 @@ RCT_EXPORT_METHOD(findAlbatross:(NSInteger)playerID
 				rate = 1.0;
 				[self.players addObject:player];
 			}
-			
+
 			[player setDelegate:self];
 			player.numberOfLoops = -1;
 			player.enableRate = YES;
 			player.rate = rate;
-			
+
 			if (error) {
-				reject(@"no_albatross", @"albatross not found", error); 
+				reject(@"no_albatross", @"albatross not found", error);
 			} else {
 				[player prepareToPlay];
 				[player play];
 				resolve(@[@{@"duration": @(player.duration), @"numberOfChannels": @(player.numberOfChannels)}]);
 			}
-			
+
 			return;
-			
+
 		}
 	}
-		
+
 	NSError *error;
-	reject(@"no_alabatross", @"albatross not found", error); 
-	
+	reject(@"no_alabatross", @"albatross not found", error);
+
 }
 
 - (void)interruption:(NSNotification*)notification {
-	
+
 	// get the user info dictionary
 	NSDictionary *interuptionDict = notification.userInfo;
-	
+
 	// get the AVAudioSessionInterruptionTypeKey enum from the dictionary
 	NSInteger interuptionType = [[interuptionDict valueForKey:AVAudioSessionInterruptionTypeKey] integerValue];
-	
+
 	// decide what to do based on interruption type here...
 	switch (interuptionType) {
 			case AVAudioSessionInterruptionTypeBegan:
 					NSLog(@"Audio Session Interruption case started.");
 					// fork to handling method here...
 					// EG:[self handleInterruptionStarted];
+
+					[self savePlayerStates];
+
 					break;
 
 			case AVAudioSessionInterruptionTypeEnded:
 					NSLog(@"Audio Session Interruption case ended.");
 					// fork to handling method here...
 					// EG:[self handleInterruptionEnded];
-					
+
 					[self restartPlayers];
-					
+
 					break;
 
 			default:
@@ -420,28 +434,45 @@ RCT_EXPORT_METHOD(findAlbatross:(NSInteger)playerID
 }
 
 //
+// savePlayerStates
+//
+- (void) savePlayerStates {
+
+	NSLog(@"savePlayerStates");
+
+	int i = 0;
+
+	for (AVAudioPlayer *player in self.players) {
+		NSLog(@"player %d is at %f", i, player.currentTime);
+		NSLog(@"player %d playing? : %@", i, (player.playing) > @"YES" : @"NO");
+		i++;
+	}
+
+
+}
+//
 // restartPlayers
 //
 - (void) restartPlayers {
-	
+
 	NSLog(@"restartPlayers");
-	
+
 	// set up AudioSession
 	AVAudioSession *session = [AVAudioSession sharedInstance];
-	
+
 	NSError *setCategoryError = nil;
-	
+
 	if (![session setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError]) {
 		NSLog(@"%@", [setCategoryError localizedDescription]);
 	}
-	
+
 	for (AVAudioPlayer *player in self.players) {
 		NSLog(@"player x is at %f", player.currentTime);
 		if (player.playing == NO) {
 			[player play];
 		}
 	}
-	
+
 }
 
 @end
--- jsx/TrackPlayer.js
+++ jsx/TrackPlayer.js
@@ -1,4 +1,4 @@
-import React, { 
+import React, {
 	Component
 } from 'react';
 
@@ -21,33 +21,41 @@ var JRMultiTrackPlayer = require('NativeModules').JRMultiTrackPlayer;
 export class TrackPlayer extends Component {
 	constructor(props) {
 		super(props);
-		
+
 		console.log('TrackPlayer init ' + Math.random() * 1000);
-		
+
 		this.rateSliderDefaults = {
 			minimumValue: 0.5,
 			maximumValue: 2.0,
 			step: 0.2
 		};
-		
+
 		this.volSliderDefaults = {
 			minimumValue: 0,
 			maximumValue: 1.0,
 			step: 0.2
 		};
-		
+
 		this.panSliderDefaults = {
 			minimumValue: -1.0,
 			maximumValue: 1.0,
 			step: 0.25
 		};
-		
+
+		this.timeSliderDefaults = {
+			minimumValue: 0,
+			maximumValue: 1.0,
+			step: 0.01
+		};
+
 		this.state = {
 			rateSliderDisabled: true,
 			rateSliderVal: 1.0,
 			volSliderDisabled: true,
-			volSliderVal: 1.0,
 			panSliderDisabled: true,
+			timeSliderDisabled: true,
+			volSliderVal: 1.0,
+			timeSliderVal: 0,
 			panSliderVal: 0,
 			songPlaying: false,
 			songInfo: {
@@ -55,7 +63,7 @@ export class TrackPlayer extends Component {
 				artist: ''
 			}
 		};
-		
+
 		/*if (this.props.index == 0) {
 			JRMultiTrackPlayer.findAlbatross(this.props.index).then((success) => {
 				console.log('found albatross :)');
@@ -64,15 +72,15 @@ export class TrackPlayer extends Component {
 			});
 		}*/
 	}
-	
+
 	componentWillMount() {
-		
-		this.subscription = NativeAppEventEmitter.addListener('SongPlaying', (evt) => {
-			
+
+		this.playerSubscription = NativeAppEventEmitter.addListener('SongPlaying', (evt) => {
+
 			if (evt.player == this.props.index) {
-				
+
 				console.log('SongPlaying', evt);
-				
+
 				this.setState({
 					songPlaying: true,
 					songInfo: {
@@ -82,99 +90,117 @@ export class TrackPlayer extends Component {
 					rateSliderDisabled: false,
 					volSliderDisabled: false,
 					panSliderDisabled: false,
-				})
+				});
+
+				this.playingSubscription = NativeAppEventEmitter.addListener('PlayingUpdate', (evt) => {
+
+					console.log('PlayingUpdate', evt);
+
+					var i = 0,
+					    update;
+					for(i;i<evt.length;i++) {
+						update  = evt[i];
+						console.log(update);
+						if (update.player == this.props.index) {
+							this.setState({
+								timeSliderVal: update.currentTime
+							});
+						}
+					}
+				});
 			}
 		});
 	}
-	
+
 	componentWillUnmount() {
-		
+
 		JRMultiTrackPlayer.stopPlayerByID(this.props.index).then((success) => {
 			console.log('stopPlayerByID success', success);
 		}, (error) => {
 			console.error('stopPlayerByID error', error);
 		});
-		
-		if (this.subscription) this.subscription.remove();
-		
+
+		if (this.playerSubscription) this.playerSubscription.remove();
+		if (this.playingSubscription) this.playingSubscription.remove();
+
 	}
-	
+
 	_onRateUpdate(val) {
-		
+
 		this.setState({rateSliderVal: val});
-		
+
 		JRMultiTrackPlayer.setRateAsync(val, this.props.index).then((success) => {
 			//console.log('setRate success', success);
 		}, (error) => {
 			console.error('setRate error', error);
 		});
 	}
-	
+
 	_onVolUpdate(val) {
-		
+
 		this.setState({volSliderVal: val});
-		
+
 		JRMultiTrackPlayer.setVolAsync(val, this.props.index).then((success) => {
 			//console.log('setVol success', success);
 		}, (error) => {
 			console.error('setVol error', error);
 		});
 	}
-	
+
 	_onPanUpdate(val) {
-		
+
 		this.setState({panSliderVal: val});
-		
+
 		JRMultiTrackPlayer.setPanAsync(val, this.props.index).then((success) => {
 			//console.log('setVol success', success);
 		}, (error) => {
 			console.error('setPan error', error);
 		});
 	}
-	
+
 	_onSelectButtonClick() {
 		JRMultiTrackPlayer.showPicker(this.props.index);
 	}
-	
+
 	render() {
-		
+
 		var {height, width} = Dimensions.get('window');
 		var playing = this.state.songPlaying;
-		var playingText = (!this.state.songPlaying) ? '' : 
-		                                               this.state.songInfo.artist + 
+		var playingText = (!this.state.songPlaying) ? '' :
+		                                               this.state.songInfo.artist +
 		                                               ' "' + this.state.songInfo.title + '"';
 		var buttonText = (!this.state.songPlaying) ? 'pick a song' : 'change song';
 		var currentRate = 'Rate: ' + this.state.rateSliderVal.toFixed(2);
 		var currentVol = 'Vol: ' + this.state.volSliderVal.toFixed(2);
 		var currentPan = 'Pan: ' + this.state.panSliderVal.toFixed(2);
-		
+
 		return (
 			<View style={JRTrackPlayerStyles.container}>
 				<Text style={JRTrackPlayerStyles.playing}>{playingText}</Text>
 				<Text style={JRTrackPlayerStyles.info}>{''}</Text>
-				<Slider 
-					style={JRTrackPlayerStyles.slider}
-					{...this.rateSliderDefaults}
-					value = {0}
-					disabled = {true}/>
+				<Slider
+					style={JRTrackPlayerStyles.slider, JRTrackPlayerStyles.timeSlider}
+					{...this.timeSliderDefaults}
+					value = {this.state.timeSliderVal}
+					disabled = {this.state.timeSliderDisabled}/>
 				<View style={JRTrackPlayerStyles.controlsRow}>
 					<View style={JRTrackPlayerStyles.slidersContainer}>
 						<Text style={JRTrackPlayerStyles.info}>{currentRate}</Text>
-						<Slider 
+						<Slider
 							style={JRTrackPlayerStyles.slider}
 							{...this.rateSliderDefaults}
 							value = {this.state.rateSliderVal}
 							disabled = {this.state.rateSliderDisabled}
 							onSlidingComplete={this._onRateUpdate.bind(this)} />
 						<Text style={JRTrackPlayerStyles.info}>{currentVol}</Text>
-						<Slider 
+						<Slider
 							style={JRTrackPlayerStyles.slider}
 							{...this.volSliderDefaults}
 							value = {this.state.volSliderVal}
 							disabled = {this.state.volSliderDisabled}
 							onSlidingComplete={this._onVolUpdate.bind(this)} />
 						<Text style={JRTrackPlayerStyles.info}>{currentPan}</Text>
-						<Slider 
+						<Slider
 							style={JRTrackPlayerStyles.slider}
 							{...this.panSliderDefaults}
 							value = {this.state.panSliderVal}
@@ -182,17 +208,17 @@ export class TrackPlayer extends Component {
 							onSlidingComplete={this._onPanUpdate.bind(this)} />
 					</View>
 					<View style={JRTrackPlayerStyles.buttonsContainer}>
-						<AlbaButton 
+						<AlbaButton
 						 onPress={this.props.onHide}
 						 text={'- remove track'} />
-						<AlbaButton 
+						<AlbaButton
 						 onPress={this._onSelectButtonClick.bind(this, this.props.index)}
 						 text={buttonText} />
 					</View>
 				</View>
 			</View>
 		);
-		
+
 	}
 }
 TrackPlayer.propTypes = {
@@ -200,7 +226,7 @@ TrackPlayer.propTypes = {
 	onHide: React.PropTypes.func.isRequired,
 };
 TrackPlayer.defaultProps = {
-	
+
 };
 
 let { height, width } = Dimensions.get('window');
@@ -238,6 +264,9 @@ const JRTrackPlayerStyles = StyleSheet.create({
 	slider: {
 		height: 18,
 	},
+	timeSlider: {
+		width: ww,
+	}
 	butttonsContainer: {
 		width: .35 * ww
 	}
